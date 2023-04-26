e802e2d10abe207596d682998dba6e979c6f084b
Antoinette Barrett <antoinet@su-c4.msk.21-school.ru>
2023-04-26 20:20:19 +0300

    day02

---
+++ day02/script1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let currentUser = prompt("What is yuor name?");
+let welcomeMessage = "If you waited all your life for the sign, here it is 🪧";
+
+alert("Hello! " + currentUser);
+alert(welcomeMessage);
+result = confirm("Whould you like to see the page?");
+
